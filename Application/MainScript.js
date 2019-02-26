c94417e4c8362323dc997315b91220ba46dc8bd3
Lumbridge <ryansainty555@hotmail.com>
2019-02-26 21:58:46 +0000

    Updated to include new shortcuts & styling.

--- Application/MainScript.js
+++ Application/MainScript.js
@@ -1,14 +1,35 @@
 const $ = require('jquery');
+const { ipcRenderer } = require('electron');
 
-$(function(){
-  $("img").on("click", function(){
+$(function () {
+
+  // listen for showPhase messages from main.js
+  ipcRenderer.on('showPhase', (event, phase) => {
+    // show all phases
+    if (phase === "all") {
+      // show all phase images
+      $("img").show();
+    } 
+    // show a specific phase
+    else {
+      // hide all images
+      $("img").hide();
+      // show only the selected phase
+      $("#" + phase).show();
+    }
+  });
+
+  // click event for phase images
+  $("img").on("click", function () {
     // hide all images
     $("img").hide();
     // show only the one we clicked
     $(this).show();
   });
 
-  $("#resetButton").on("click", function(){
+  // reset button click event
+  $("#resetButton").on("click", function () {
+    // show all phase images
     $("img").show();
   });
-})
+});
--- main.js
+++ main.js
@@ -2,18 +2,18 @@ const electron = require('electron');
 const path = require('path');
 const url = require('url');
 
-const { app, BrowserWindow, Menu } = electron;
+const { app, BrowserWindow, Menu, globalShortcut } = electron;
 
 process.env.NODE_ENV = 'production';
 
 let win;
 
 // open window when loaded
-app.on('ready', function(){
+app.on('ready', function () {
   // create window object
   win = new BrowserWindow({
-    width:715,
-    height:750,
+    width: 715,
+    height: 750,
     icon: __dirname + '/assets/icons/win/icon.ico',
     resizable: false
   });
@@ -26,11 +26,32 @@ app.on('ready', function(){
   }));
 
   // open dev tools for debugging
-  // win.webContents.openDevTools();
+  //win.webContents.openDevTools();
 
   // delete window on close
   win.on('closed', () => {
     win = null;
+  });
+
+  // Phase A image shortcut
+  globalShortcut.register('Shift+Ctrl+1', () => {
+    win.webContents.send('showPhase', 'phaseA');
+  });
+  // Phase B image shortcut
+  globalShortcut.register('Shift+Ctrl+2', () => {
+    win.webContents.send('showPhase', 'phaseB');
+  });
+  // Phase C image shortcut
+  globalShortcut.register('Shift+Ctrl+3', () => {
+    win.webContents.send('showPhase', 'phaseC');
+  });
+  // Phase D image shortcut
+  globalShortcut.register('Shift+Ctrl+4', () => {
+    win.webContents.send('showPhase', 'phaseD');
+  });
+  // Show all 
+  globalShortcut.register('Shift+Ctrl+X', () => {
+    win.webContents.send('showPhase', 'all');
   });
 
   // create menu
@@ -43,7 +64,7 @@ app.on('ready', function(){
 const mainMenuTemplate = [
   {
     label: 'File',
-    submenu:[
+    submenu: [
       {
         label: 'Quit',
         accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
@@ -57,7 +78,7 @@ const mainMenuTemplate = [
 
 // quit when all windows closed
 app.on('window-all-closed', () => {
-  if(process.platform !== 'darwin'){
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
